2a68009b94018d0171aa2d408c53a4d949389fc3
VladSmirnou <smr.vladislav@gmail.com>
2024-07-29 00:32:29 +0300

    video 19

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import { Header } from './components/header/Header';
 import { NavBar } from './components/navBar/NavBar';
 import { Profile } from './components/profile/Profile';
 import { Dialogs } from './components/dialogs/Dialogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className={'app-wrapper'}>
-      <Header />
-      <NavBar />
-      <div className={'app-wrapper-content'}>
-        {/* <Profile /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className={'app-wrapper'}>
+        <Header />
+        <NavBar />
+        <div className={'app-wrapper-content'}>
+          <Routes>
+            <Route path='/profile' Component={Profile}/>
+            <Route path='/dialogs' Component={Dialogs}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 

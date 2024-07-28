c67cea3791336edbe97ce58d5e3190bbec1d6fa7
VladSmirnou <smr.vladislav@gmail.com>
2024-07-28 19:16:11 +0300

    initial

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+export const Profile = () => {
+  return (
+    <div className={'content'}>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>  
+  )
+}
